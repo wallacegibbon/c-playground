b7eb6f093c1cbd157f767074f4f99151d5b757ba
wallace <wallacegibbon@outlook.com>
2022-01-14 08:21:56 +0800

    .

---
+++ sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef int (*sortfn)(void *ele1, void *ele2);
+
+void bubble_sort(void **arr) {
+}
+
+void select_sort(void **arr) {
+}
+
+void shell_sort(void **arr) {
+}
+
+void qsort(void **arr) {
+}
+
+int main(int argc, char **argv) {
+	return 0;
+}
+
