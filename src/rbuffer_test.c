4c928922468a7df0a687a7b6d0675f5f972ab518
wallace <wallacegibbon@outlook.com>
2022-02-03 17:11:58 +0800

    bug fix, one variable in the ring buffer test is too small to hold the result (32/64 bit proble)

--- src/rbuffer_test.c
+++ src/rbuffer_test.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include "rbuffer.h"
 
+// put one data into the buffer and then take one out.
+void shift_data(struct rbuffer *b, void *data) {
+	int r;
+	r = rbuffer_put(b, &data, 1);
+	//printf("r is %d\n", r);
+	//rbuffer_inspect(b);
+	rbuffer_print(b, rbuffer_fmt_raw);
+
+	long t;
+	rbuffer_get(b, (void **) &t, 1);
+}
+
 int main(int argc, const char **argv) {
+	long a[] = {0x10, 0x20, 0x30, 0x40, 0x50};
 	struct rbuffer *b = rbuffer_new(4);
-	int r, t;
 
-	long a[] = {10, 20, 30, 40, 50};
-
+	int r;
 	r = rbuffer_put(b, (void **) a, 2);
 	printf("r is %d\n", r);
 
@@ -20,15 +31,14 @@ int main(int argc, const char **argv) {
 	rbuffer_inspect(b);
 	rbuffer_print(b, rbuffer_fmt_raw);
 
+	long t;
 	r = rbuffer_get(b, (void **) &t, 1);
-	printf("get from buffer: %d\n", t);
+	printf("get from buffer: 0x%lx\n", t);
+	rbuffer_print(b, rbuffer_fmt_raw);
 
-	for (int i = 111; i < 141; i++) {
-		r = rbuffer_put(b, (void **) &i, 1);
-		//rbuffer_inspect(b);
-		rbuffer_print(b, rbuffer_fmt_raw);
-		rbuffer_get(b, (void **) &t, 1);
-	}
+	for (long i = 0x111; i < 0x121; i++)
+		shift_data(b, (void *) i);
 
 	return 0;
 }
+
