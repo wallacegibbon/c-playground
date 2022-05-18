7c0aedce7ca4addc77e99b9df0cd317db677e32c
wallace <wallacegibbon@outlook.com>
2022-05-18 16:30:35 +0800

    example for the unusual struct and array literal creating syntax

---
+++ src/struct_array_literal_syntax.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/// you can always use "{}" to create array or struct literals on stack if
+/// you give the right type information.
+
+struct blah { int a, b, c; };
+
+void print_blah(struct blah *v) {
+    printf("struct blah { %d, %d, %d }\n", v->a, v->b, v->c);
+}
+
+int main() {
+    printf("%s\n", (char[]) {97, 98, 99, 0});
+    print_blah(&(struct blah) {1, 2, 3});
+    return 0;
+}
